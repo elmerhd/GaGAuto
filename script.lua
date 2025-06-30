3e3af4d34098beb059a0369e1d1b3d10d6187f89
Elmer Duron <elmerhduron@gmail.com>
2025-07-01 04:29:59 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,27 @@
+-- Wait until the game is fully loaded and the LocalPlayer is available
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+-- Map of PlaceIds to corresponding script URLs
+local scripts = {
+    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V2.lua",
+}
+
+-- Get the script URL for the current game
+local url = scripts[game.PlaceId]
+
+-- If a script URL exists for this PlaceId, try to load and execute it
+if url then
+    local success, response = pcall(game.HttpGet, game, url)
+    if success and response then
+        local loadedFunction, loadErr = loadstring(response)
+        if loadedFunction then
+            loadedFunction()
+        else
+            warn("Failed to load string: " .. tostring(loadErr))
+        end
+    else
+        warn("HTTP Get failed: " .. tostring(response))
+    end
+else
+    warn("No script found for this PlaceId: " .. tostring(game.PlaceId))
+end
